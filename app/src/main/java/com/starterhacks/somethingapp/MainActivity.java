031ff4c64d0d51ab7558acb0dabed396e957d9d9
Kamran Tayyab <Kamran.tayyab99@gmail.com>
2018-03-03 22:05:51 -0500

    Added clarifai

--- app/src/main/java/com/starterhacks/somethingapp/MainActivity.java
+++ app/src/main/java/com/starterhacks/somethingapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.starterhacks.somethingapp;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private String key = "fd002d93741144b9840ce07a3b7e7f0c";
+    ClarifaiClient client = new ClarifaiBuilder(key).buildSync();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                final List<ClarifaiOutput<Concept>> predictionResults =
+                        client.getDefaultModels().generalModel()
+                                .predict()
+                                .withInputs(
+                                        ClarifaiInput.forImage("https://i.imgur.com/Yywr5Yc.png"))
+                                .executeSync()
+                                .get();
+                String myVar = predictionResults.toString();
+                Toast.makeText(MainActivity.this, myVar, Toast.LENGTH_SHORT).show();
             }
         });
     }
